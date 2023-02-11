dfa4d11f4d4a4ffdf0d97fda12a81f95884d34a6
John U <arjorb@gmail.com>
2023-02-11 16:58:32 +0200

    feat:done woking on the card item

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,14 +5,14 @@ const CardItem = ({ img, rating, reviewCount, country, title, price }) => {
     <>
       <div>
         <div>
-          <img src={`./${img}`} alt={title} className='w-48' />
+          <img src={`./${img}`} alt={title} className='w-44' />
         </div>
         <div className=' text-[12px] space-y-1'>
           <div className='flex gap-1 mt-2 items-center'>
             <img src='./star.svg' alt='star' />
-            <p>{rating}</p>
-            <p className='text-gray-400'>{reviewCount} . </p>
-            <p className='text-gray-400'>{country}</p>
+            <span>{rating}</span>
+            <span className='text-gray-400'>{reviewCount} . </span>
+            <span className='text-gray-400'>{country}</span>
           </div>
           <p className='font-light'>{title}</p>
           <p className='font-light'>
