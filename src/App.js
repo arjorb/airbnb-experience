60e031b19028ee768f57ec1d98fb8821a2a47bbb
John U <arjorb@gmail.com>
2023-02-11 11:03:21 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 className='text-3xl font-bold text-[#056b85] underline'>Seting up the environment!</h1>
+    </div>
+  );
+};
+
+export default App;
